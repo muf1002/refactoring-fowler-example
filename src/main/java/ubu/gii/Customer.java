52793744b706d598c83ea067111f0bada4a04d70
Marcos <muf1002@alu.ubu.es>
2024-05-16 17:25:50 +0200

    #2 Remove Temp field - thisAmount

--- src/main/java/ubu/gii/Customer.java
+++ src/main/java/ubu/gii/Customer.java
@@ -36,17 +36,16 @@ public class Customer {
 		Iterator<Rental> rentals = _rentals.iterator();
 		String result = "Rental Record for " + getName() + "\n";
 		while (rentals.hasNext()) {
-			double thisAmount = 0;
+
 			Rental each = rentals.next();
-			// determine amounts for each line
-			thisAmount = each.getCharge();
+
 			
 			// add frequent renter points
 			frequentRenterPoints += each.getFrequentRenterPoints();
 			// show figures for this rental
 			result += "\t" + each.getMovie().getTitle() + "\t"
-					+ String.valueOf(thisAmount) + "\n";
-			totalAmount += thisAmount;
+					+ String.valueOf(each.getCharge()) + "\n";
+			totalAmount += each.getCharge();
 		}
 		// add footer lines
 		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
