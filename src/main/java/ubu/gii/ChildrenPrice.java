e2799e38c65a8a26f76364c606b60c60a7b17b5e
dmp1002 <dmp1002@alu.ubu.es>
2024-05-16 17:58:06 +0200

    #3 Replace Type Code with State/Strategy on Movie

---
+++ src/main/java/ubu/gii/ChildrenPrice.java
@@ -0,0 +1,11 @@
+package ubu.gii;
+
+public class ChildrenPrice extends Price {
+
+	@Override
+	public int getPriceCode() {
+		
+		return  Movie.CHILDRENS;
+	}
+
+}
---
+++ src/main/java/ubu/gii/RegularPrice.java
@@ -0,0 +1,11 @@
+package ubu.gii;
+
+public class RegularPrice extends Price {
+
+	@Override
+	public int getPriceCode() {
+		
+		return Movie.REGULAR;
+	}
+
+}
